8e5c77c6ef11b86659793bff9f241098a89369c1
Tywayne <tywaynebuchanan@gmail.com>
2021-11-05 15:48:30 -0500

    feature: added nav, menu, section to web app

---
+++ js/index.js
@@ -0,0 +1,31 @@
+document.getElementById('root').innerHTML = `
+
+<div class="container">
+    <nav>Nav</nav>
+    <menu>
+        <button><ion-icon name="create-outline" class="icons"></ion-icon>New</button>
+        <button><ion-icon name="arrow-undo-outline"  class="icons"></ion-icon>Reply</button>
+        <button><ion-icon name="arrow-redo-outline"  class="icons"></ion-icon>Forward</button>
+        <button><ion-icon name="bookmark-outline"  class="icons"></ion-icon>Mark as unread</button>
+        <button><ion-icon name="trash-outline"></ion-icon>Trash</button>
+    </menu>
+    <section>
+        <div class="email">Email 1</div>
+        <div class="email">Email 1</div>
+        <div class="email">Email 1</div>
+        <div class="email">Email 1</div>
+        <div class="email">Email 1</div>
+        <div class="email">Email 1</div>
+        <div class="email">Email 1</div>
+        <div class="email">Email 1</div>
+        <div class="email">Email 1</div>
+        <div class="email">Email 1</div>
+        <div class="email">Email 1</div>
+        <div class="email">Email 1</div>
+    </section>
+    <main>Email</main>
+    <aside>Additional Information</aside>   
+</div>
+
+
+`;
